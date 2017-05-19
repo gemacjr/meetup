9c6f998d05d61d50fd066c5c18a17ff18876fd86
Ed <edmccormic@gmail.com>
2017-05-18 21:36:41 -0600

    Update backend

---
+++ meetup-backend/server/modules/meetups/model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from 'mongoose';
+
+const MeetupSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: [5, 'at least 5 characters long']
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: [10, 'at least 10 characters long']
+  },
+  eventDate: {
+    type: Date
+  },
+  group: {
+    type: Schema.Types.ObjectId,
+    ref: 'Group'
+  }
+}, { timestamps: true });
+
+export default mongoose.model('Meetup', MeetupSchema);
